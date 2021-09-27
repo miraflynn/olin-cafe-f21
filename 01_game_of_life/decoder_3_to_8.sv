83a0cf371e9c2795c927eb4256e187a3bdb8846f
Mira Flynn <miraflynn0@gmail.com>
2021-09-27 18:31:28 -0400

    made a 3 to 8 decoder

--- 01_game_of_life/decoder_3_to_8.sv
+++ 01_game_of_life/decoder_3_to_8.sv
@@ -3,5 +3,16 @@ module decoder_3_to_8(ena, in, out);
   input wire ena;
   input wire [2:0] in;
   output logic [7:0] out;
+  
+  always_comb begin
+    out[7] = ena & in[2] & in[1] & in[0];
+    out[6] = ena & in[2] & in[1] & ~in[0];
+    out[5] = ena & in[2] & ~in[1] & in[0];
+    out[4] = ena & in[2] & ~in[1] & ~in[0];
+    out[3] = ena & ~in[2] & in[1] & in[0];
+    out[2] = ena & ~in[2] & in[1] & ~in[0];
+    out[1] = ena & ~in[2] & ~in[1] & in[0];
+    out[0] = ena & ~in[2] & ~in[1] & ~in[0];
+  end
 
 endmodule
