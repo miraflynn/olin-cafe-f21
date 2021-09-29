df6cac944f8658ae434a91bc712e51a6aa0322e1
Mira Flynn <miraflynn0@gmail.com>
2021-09-29 17:13:56 -0400

    add 2 to 4 decoder

--- 01_game_of_life/decoder_2_to_4.sv
+++ 01_game_of_life/decoder_2_to_4.sv
@@ -4,5 +4,11 @@ module decoder_2_to_4(ena, in, out);
   input wire [1:0] in;
   output logic [3:0] out;
 
+  always_comb begin
+    out[3] = ena & in[1] & in[0];
+    out[2] = ena & in[1] & ~in[0];
+    out[1] = ena & ~in[1] & in[0];
+    out[0] = ena & ~in[1] & ~in[0];
+  end
 
 endmodule
