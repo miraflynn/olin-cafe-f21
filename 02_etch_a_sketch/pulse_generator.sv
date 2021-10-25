f26ccce7cbe24e003ee644a875a97fcfadda9085
Mira Flynn <miraflynn0@gmail.com>
2021-10-25 17:30:06 -0400

    Copied stuff to lab 2 from hw5

--- 02_etch_a_sketch/pulse_generator.sv
+++ 02_etch_a_sketch/pulse_generator.sv
@@ -2,6 +2,8 @@
   Outputs a pulse generator with a period of "ticks".
   out should go high for one cycle ever "ticks" clocks.
 */
+
+// Taken from lab 2
 module pulse_generator(clk, rst, ena, ticks, out);
 
 parameter N = 8;
@@ -21,12 +23,6 @@ always_ff @( posedge clk) begin : pulsegen
   end else if (ena) begin
     counter <= counter + 1;
   end
-// counter = counter + 1;
-
-// out = ticks == counter;
-// if(out)
-//   counter = 0;
-// end
 end
 
 always_comb counter_comparator = counter >= (ticks - 1);
--- 02_etch_a_sketch/pwm.sv
+++ 02_etch_a_sketch/pwm.sv
@@ -12,6 +12,8 @@ input wire step; // Enables the internal counter. You should only increment when
 input wire [N-1:0] duty; // The "duty cycle" input.
 output logic out;
 
+wire [N-1:0] ticks = '1;
+
 logic [N-1:0] counter;
 
 // Create combinational (always_comb) and sequential (always_ff @(posedge clk)) 
@@ -22,6 +24,23 @@ logic [N-1:0] counter;
 // bonus: out should be fully zero at duty = 0, and fully 1 (always on) at duty = 2^N-1;
 // You can use behavioural combinational logic, but try to keep your sequential
 //   and combinational blocks as separate as possible.
+
+logic counter_comparator;
+logic tick_rst;
+
+always_comb tick_rst = rst | counter_comparator;
+
+always_ff @( posedge step, posedge rst) begin : pulsegen
+  if(tick_rst) begin
+    counter <= 0;
+  end else if (ena) begin
+    counter <= counter + 1;
+  end
+end
+
+always_comb counter_comparator = counter >= (ticks-1);
+
+always_comb out = counter < duty;
 
 
 endmodule
