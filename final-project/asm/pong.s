7e3d83263bbabfd68954d658ccf6189af818a730
nolanflynn <nolanflynn0@gmail.com>
2021-12-17 15:50:13 -0500

    Ball bounces correctly for all 8 directions

--- final-project/asm/pong.s
+++ final-project/asm/pong.s
@@ -6,8 +6,8 @@
 # x2/sp    :   stack pointer
 # x3/gp    :   global pointer
 # x4/tp    :   thread pointer
-# x5/t0    :   TEMP: Ball X (320)
-# x6/t1    :   TEMP: Ball Y (240)
+# x5/t0    :   TEMP: Ball Memory Pos
+# x6/t1    :   TEMP: 1 row of memory
 # x7/t2    :   TEMP: Ball Direction
 # x8/s0/fp :   saved register/frame pointer
 # x9/s1    :   saved register
@@ -51,95 +51,408 @@
 # 6: -Y
 # 7: -Y, +X
 
+# +X: add 1 row to memory address
+# +Y: add 4 to memory address
 
 RESET: 
-    and t0, zero, zero
-    and t1, zero, zero
-    and t2, zero, zero
-    # beq zero, zero, RESET
-
+    addi t0, zero, 0
+    lui t0, 1
+    slli t0, t0, 16
+    addi t6, zero, 1
+    slli t6, t6, 16
+    add t0, t0, t6 # t0 = 0x10010000
+    
+    addi, t1, zero, 16 
+    slli, t1, t1, 14 # t1 = 256 rows of memory address
+    add t0, t0, t1 # t0 = 256 rows into memory
+    addi t0, t0, -1024 # t0 = 255 rows and 256 columns into memory
+    
+    addi, t1, zero, 16 
+    slli, t1, t1, 7 # t1 = 0x00000800 = 1 row of memory address
+    
+    addi t2, zero, 5 # set starting direction
+    
+    # Draw all 0s to display
+    lui t3, 1
+    slli t3, t3, 16
+    addi t6, zero, 1
+    slli t6, t6, 16
+    add t3, t3, t6 # t3 = 0x10010000
+    
+    and t6, zero, zero # t6 = 0
+    #addi t5, zero, 75 # t5 = 75
+    #slli t5, t5, 10 # t5 = 76800 (320*240)
+    addi t5, zero, 1 # t5 = 1
+    slli t5, t5, 17 # t5 = 131072 (512*256)
+    #addi t4, zero, 255 # reset board to blue
+    addi t4, zero, 0 # reset board to black
+    RESET_LOOP_START: 
+        sw t4, (t3)
+        addi t3, t3, 4
+        addi t6, t6, 1 # t6 = t6 + 1
+        blt t6, t5, RESET_LOOP_START
+       
 STEP: 
-    and t6, zero, zero
+    addi t6, zero, 0 # color 0x00000000 = Black
+    # mem position +0 row, +0 col
+    sub t0, t0, t1 # mem position -1 row, +0 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position -1 row, -1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, 8 # mem position -1 row, +1 col
+    sw t6, (t0) # set color t6
+    add t0, t0, t1 # mem position +0 row, + 1 col
+    sw t6, (t0) # set color t6
+    add t0, t0, t1 # mem position +1 row, + 1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position +1 row, +0 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position +1 row, -1 col
+    sw t6, (t0) # set color t6
+    sub t0, t0, t1 # mem position +0 row, -1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, 4 # mem position +0 row, +0 col
+    sw t6, (t0) # set color t6
 
 
 MOVE: 
     and t6, zero, zero
     # lui t3, 0
-    beq t2, t6, STEP_0 # 24
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_1
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_2
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_3
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_4
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_5
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_6
-    #addi t6, t6, 1
-    #beq t2, t6, STEP_7 # 80
+    beq t2, t6, MOVE_0 # 24
+    addi t6, t6, 1
+    beq t2, t6, MOVE_1
+    addi t6, t6, 1
+    beq t2, t6, MOVE_2
+    addi t6, t6, 1
+    beq t2, t6, MOVE_3
+    addi t6, t6, 1
+    beq t2, t6, MOVE_4
+    addi t6, t6, 1
+    beq t2, t6, MOVE_5
+    addi t6, t6, 1
+    beq t2, t6, MOVE_6
+    addi t6, t6, 1
+    beq t2, t6, MOVE_7 # 80
 
 
-# +X   
-STEP_0: 
-    addi t0, t0, 2
-    #beq zero, zero, DRAW
-    beq zero, zero, STEP_0
+# +X: add 1 row to memory address
+# +Y: add 4 to memory address
+
+# +2X   
+MOVE_0: 
+    add t0, t0, t1 # Move
+    add t0, t0, t1
+    
+    # -X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    addi, t4, zero, 16 
+    slli, t4, t4, 15 # t4 = 512 rows of memory address
+    add t3, t3, t4 # t3 = 512 rows into memory
+    addi t3, t3, -2048 # t0 = 511 rows into memory
+    blt t3, t0, MOVE_0_BOUNCE
+    
+    beq zero, zero, DRAW
+MOVE_0_BOUNCE:
+    addi t2, zero, 4
+    sub t0, t0, t1
+    sub t0, t0, t1
+    beq zero, zero, DRAW
+    
     
 # +X, +Y
-STEP_1: 
-    addi t0, t0, 1
-    addi t1, t1, 1
+MOVE_1: 
+    add t0, t0, t1 # Move
+    addi t0, t0, 4
+    
+    # -X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    addi, t4, zero, 16 
+    slli, t4, t4, 15 # t4 = 512 rows of memory address
+    add t3, t3, t4 # t3 = 512 rows into memory
+    addi t3, t3, -2048 # t0 = 511 rows into memory
+    blt t3, t0, MOVE_1_BOUNCEX
+    
+    # -Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_1_BOUNCEY # Bounce if within 16 memory addresses from end
+    
     beq zero, zero, DRAW
 
-# +Y
-STEP_2: 
-    addi t1, t1, 2
+MOVE_1_BOUNCEX:
+    addi t2, zero, 3 # +X, +Y to -X, +Y
+    sub t0, t0, t1
+    addi t0, t0, -4 # Undo this move
+    beq zero, zero, DRAW
+
+MOVE_1_BOUNCEY:
+    addi t2, zero, 7 # +X, -Y to +X, -Y
+    sub t0, t0, t1
+    addi t0, t0, -4 # Undo this move
+    beq zero, zero, DRAW
+
+# +2Y
+MOVE_2: 
+    addi t0, t0, 8
+    
+    # -Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_2_BOUNCE # Bounce if within 16 memory addresses from end
+    beq zero, zero, DRAW
+MOVE_2_BOUNCE:
+    addi t2, zero, 6
+    addi t0, t0, -8 # Undo this move
     beq zero, zero, DRAW
 
 # -X, +Y
-STEP_3: 
-    addi t0, t0, -1
-    addi t1, t1, 1
+MOVE_3: 
+    sub t0, t0, t1
+    addi t0, t0, 4
+    
+    # +X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    blt t0, t3, MOVE_3_BOUNCEX
+    
+    # -Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_3_BOUNCEY # Bounce if within 16 memory addresses from end
+    
     beq zero, zero, DRAW
 
-# -X
-STEP_4: 
-    addi t0, t0, -2 # 128
+MOVE_3_BOUNCEX:
+    addi t2, zero, 1 # -X, +Y to +X, +Y
+    add t0, t0, t1
+    addi t0, t0, -4 # Undo this move
+    beq zero, zero, DRAW
+
+MOVE_3_BOUNCEY:
+    addi t2, zero, 5 # -X, +Y to -X, -Y
+    add t0, t0, t1
+    addi t0, t0, -4 # Undo this move
+    beq zero, zero, DRAW
+
+
+# -2X
+MOVE_4: 
+    sub t0, t0, t1
+    sub t0, t0, t1
+    
+    # +X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    blt t0, t3, MOVE_4_BOUNCE
+    
+    beq zero, zero, DRAW
+MOVE_4_BOUNCE:
+    addi t2, zero, 0
+    add t0, t0, t1
+    add t0, t0, t1
     beq zero, zero, DRAW
 
 # -X, -Y
-STEP_5: 
-    addi t0, t0, -1
-    addi t1, t1, -1
+MOVE_5: 
+    sub t0, t0, t1
+    addi t0, t0, -4
+    
+    # +X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    blt t0, t3, MOVE_5_BOUNCEX
+    
+    # +Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_5_BOUNCEY # Bounce if within 16 memory addresses from end
+    
     beq zero, zero, DRAW
 
-# -Y
-STEP_6: 
-    and t6, zero, zero # 148
-    addi t1, t1, -2
-    lui t3, 0
-    beq zero, zero, DRAW # 156
-
-# -Y, +X
-STEP_7: 
-    addi t0, t0, 1
-    addi t1, t1, -1
+MOVE_5_BOUNCEX:
+    addi t2, zero, 7 # -X, -Y to +X, -Y
+    add t0, t0, t1
+    addi t0, t0, 4 # Undo this move
+    beq zero, zero, DRAW
+    
+MOVE_5_BOUNCEY:
+    addi t2, zero, 3 # -X, -Y to -X, +Y
+     add t0, t0, t1
+    addi t0, t0, 4 # Undo this move
     beq zero, zero, DRAW
 
+# -2Y
+MOVE_6: 
+    addi t0, t0, -8
+    
+    # +Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_6_BOUNCE # Bounce if within 16 memory addresses from end
+    
+    beq zero, zero, DRAW
+MOVE_6_BOUNCE:
+    addi t2, zero, 2
+    addi t0, t0, 8 # Undo this move
+    beq zero, zero, DRAW
+
+# +X, -Y
+MOVE_7: 
+    add t0, t0, t1
+    addi t0, t0, -4
+    
+    # -X bounce
+    addi t3, zero, 0
+    lui t3, 1
+    slli t3, t3, 16
+    addi t4, zero, 1
+    slli t4, t4, 16
+    add t3, t3, t4 # t3 = 0x10010000
+    
+    addi, t4, zero, 16 
+    slli, t4, t4, 15 # t4 = 512 rows of memory address
+    add t3, t3, t4 # t3 = 512 rows into memory
+    addi t3, t3, -2048 # t0 = 511 rows into memory
+    blt t3, t0, MOVE_7_BOUNCEX
+    
+    # +Y bounce
+    addi t4, zero, 2047 # mask for only position in row
+    and t3, t0, t4 # t3 = position in row
+    sub t3, t1, t3 # t3 = distance from edge
+    #ebreak
+    addi t4, zero, 16
+    blt t3, t4, MOVE_7_BOUNCEY # Bounce if within 16 memory addresses from end
+    
+    beq zero, zero, DRAW
+    
+MOVE_7_BOUNCEX:
+    addi t2, zero, 5 # +X, -Y to -X, +Y
+    sub t0, t0, t1
+    addi t0, t0, 4 # Undo this move
+    beq zero, zero, DRAW
+
+MOVE_7_BOUNCEY:
+    addi t2, zero, 1 # +X, -Y to +X, +Y
+    sub t0, t0, t1
+    addi t0, t0, 4 # Undo this move
+    beq zero, zero, DRAW
 
 DRAW: 
-    and t3, zero, zero
-    lui s0, 0 # 172
-    #beq zero, zero, DRAW
-    #beq zero, zero, RESET
+    addi t6, zero, -1 # color 0xFFFFFFFF = White
+    # mem position +0 row, +0 col
+    sub t0, t0, t1 # mem position -1 row, +0 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position -1 row, -1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, 8 # mem position -1 row, +1 col
+    sw t6, (t0) # set color t6
+    add t0, t0, t1 # mem position +0 row, + 1 col
+    sw t6, (t0) # set color t6
+    add t0, t0, t1 # mem position +1 row, + 1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position +1 row, +0 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, -4 # mem position +1 row, -1 col
+    sw t6, (t0) # set color t6
+    sub t0, t0, t1 # mem position +0 row, -1 col
+    sw t6, (t0) # set color t6
+    addi t0, t0, 4 # mem position +0 row, +0 col
+    sw t6, (t0) # set color t6
+    
+    addi t6, zero, 1
+    slli t6, t6, 10
+    addi t5, zero, 0
+    DRAW_DELAY_LOOP:
+        addi t5, t5, 1
+        blt t5, t6, DRAW_DELAY_LOOP
+    
+    beq zero, zero, STEP
+    
+END:
+and zero, zero, zero
+
+
+
+#    # Draw all 0s to display
+#    lui t3, 1
+#    slli t3, t3, 16
+#    addi t6, zero, 1
+#    slli t6, t6, 16
+#    add t3, t3, t6 # t3 = 0x10010000
+#    
+#    and t6, zero, zero # t6 = 0
+#    #addi t5, zero, 75 # t5 = 75
+#    #slli t5, t5, 10 # t5 = 76800 (320*240)
+#    addi t5, zero, 1 # t5 = 1
+#    slli t5, t5, 17 # t5 = 131072 (512*256)
+#    LOOP_START_1: 
+#        sw zero, (t3)
+#        addi t3, t3, 4
+#        addi t6, t6, 1 # t6 = t6 + 1
+#        blt t6, t5, LOOP_START_1
+#    
+#    # Draw all 0xFFFFFFFFs to display
+#    lui t3, 1
+#    slli t3, t3, 16
+#    addi t6, zero, 1
+#    slli t6, t6, 16
+#    add t3, t3, t6
+#    
 #    and t6, zero, zero # t6 = 0
 #    addi t5, zero, 75 # t5 = 75
 #    slli t5, t5, 10 # t5 = 76800 (320*240)
-#    LOOP_START: 
-#        sw t0, 0(zero)
+#    addi t5, zero, 1 # t5 = 1
+#    slli t5, t5, 17 # t5 = 131072 (512*256)
+#    addi t4, zero, -1 # t4 = -1 = 0xFFFFFFFF
+#    
+#
+#
+#    LOOP_START_2: 
+#        sw t4, (t3)
+#        addi, t3, t3, 4
 #        addi t6, t6, 1 # t6 = t6 + 1
-#        blt t6, t5, LOOP_START
+#        blt t6, t5, LOOP_START_2
+#        
 #    beq zero, zero, DRAW
